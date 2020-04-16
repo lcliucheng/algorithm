edf25f3086697dab05749c3c2a75b22ec5d23ad9
user1 <sss@s.com>
2020-04-16 23:08:18 +0800

    合并算法

--- algorithm-demo-test/src/main/java/com/lc/algorithm/merge/MergeDemo.java
+++ algorithm-demo-test/src/main/java/com/lc/algorithm/merge/MergeDemo.java
@@ -1,10 +1,9 @@
 package com.lc.algorithm.merge;
 
-import com.sun.jmx.remote.internal.ArrayQueue;
-
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
 import java.util.Queue;
-import java.util.Stack;
 
 /**
  * 合并算法
@@ -15,28 +14,48 @@ public class MergeDemo {
      * 输入: [[1,3],[2,6],[8,10],[15,18]]
      * 输出: [[1,6],[8,10],[15,18]]
      * 解释: 区间 [1,3] 和 [2,6] 重叠, 将它们合并为 [1,6].
+     *
+     * @return
      */
     public static int[][] merge(int[][] intervals) {
-        ArrayQueue<int[]> queue = new ArrayQueue<int[]>(Integer.MAX_VALUE);
-        for(int i=0;i<intervals.length;i++){
-            int [] temp=intervals[i];
+        Queue<int[]> queue = new LinkedList<int[]>();
+        for (int i = 0; i < intervals.length; i++) {
+            int[] temp = intervals[i];
             queue.add(temp);
         }
+        ArrayList<int[]> ints = new ArrayList<int[]>();
+        int[] tmep = queue.poll();
+        while (!queue.isEmpty()) {
+            int[] next = queue.poll();
+            int end = tmep[1];
+            if (end >= next[0] && end <= next[1]) {
+                int[] newArr = {tmep[0], next[1]};
 
-
-     /*   for(int i=1;i<queue.size();i++){
-            int[] ints1 = queue.get(i);
-            if(){
-
+               // queue.add(newArr);
+                tmep = newArr;
+            } else if (end >= next[0] && end > next[1]) {
+                int[] newArr = {tmep[0], tmep[1]};
+                //queue.add(newArr);
+                tmep = newArr;
+            } else {
+                ints.add(tmep);
+                tmep=next;
             }
-        }*/
+
+        }
+        ints.add(tmep);
 
 
-        return null;
+        return ints.toArray(new int[0][0]);
     }
 
     public static void main(String[] args) {
+        int[][] aa = {{1, 3}, {2, 6}, {8, 10}, {15, 18}};
+        int[][] merge = merge(aa);
+        for(int i=0;i<merge.length;i++){
+            System.out.println(Arrays.toString(merge[i]));
 
+        }
     }
 
 }
