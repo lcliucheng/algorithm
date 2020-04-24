13a4d4293463fb69891ebed5cacee415a32255f4
lc <admin>
2020-04-24 09:15:37 +0800

    硬币

--- algorithm-demo-test/src/main/java/com/lc/algorithm/interview/WaysToChange.java
+++ algorithm-demo-test/src/main/java/com/lc/algorithm/interview/WaysToChange.java
@@ -1,5 +1,8 @@
 package com.lc.algorithm.interview;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @author liu cheng
  * @since 2020-04-23 16:16
@@ -38,32 +41,55 @@ public class WaysToChange {
      * 0<b<n/5
      * 0<c<n/10
      * 0<e<25/e
+     * <p>
+     * <p>
+     * 动态规划转移
      */
-    static int[] aa = {5, 10, 25};
 
     public static int waysToChange(int n) {
-        int num=0;
-        int a = n;
-        int b = n / 5;
-        int c = n / 10;
-        int d = n / 25;
-        for (int i = 0; i <= a; i++) {
-            int e = (n - i * 1);
-            if(e==0){
-                num+=1;
-            }
-            if (e % 5 == 0) {
-                int g = e / 25;
+        int[][] dp=new int[4][n+1];
+        int[] coins={1,5,10,25};
 
-            }
+        for(int i=0;i<=n;i++)
+            dp[0][i]=1;
+        for(int i=0;i<4;i++)
+            dp[i][0]=1;
 
+        for(int i=1;i<4;i++){
+            for(int j=1;j<=n;j++){
+                if(j>=coins[i])
+                    dp[i][j]=(dp[i-1][j]+dp[i][j-coins[i]])%1000000007;
+                else
+                    dp[i][j]=dp[i-1][j];
+            }
         }
+        return dp[3][n];
 
-        return num;
+
+
+
     }
 
+    public int waysToChange2(int n) {
+        int[] dp=new int[n+1];
+        int[] coins={1,5,10,25};
+
+        for(int i=0;i<=n;i++)
+            dp[i]=1;
+
+        for(int i=1;i<4;i++){
+            for(int j=1;j<=n;j++){
+                if(j>=coins[i])
+                    dp[j]=(dp[j]+dp[j-coins[i]])%1000000007;
+            }
+        }
+        return dp[n];
+    }
+
+
+
     public static void main(String[] args) {
-        int i = waysToChange (61);
+        int i = waysToChange (5);
         System.out.println (i);
     }
 }
